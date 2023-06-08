10df47794f17c7467dd8f5769737f8f4fd1dd7ca
Davinder Kumar <davindertwal@gmail.com>
2023-06-08 15:33:06 +0530

    level 2 encryption

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {
+  secret: secret,
+  encryptedFields: ["password"],
+});
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+
+  newUser.save().then(() => {
+    res.render("secrets");
+  });
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username })
+    .then((data) => {
+      if (data.password === password) {
+        res.render("secrets");
+      } else {
+        res.send("Wrong password");
+      }
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
+
+app.listen(3000, () => {
+  console.log("Server Started at " + new Date().getSeconds());
+});
