91834925a95d97f19a3c6a5c544fe0c4a5007bc5
Davinder Kumar <davindertwal@gmail.com>
2023-06-08 17:08:46 +0530

    level 3 md5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const app = express();
@@ -16,10 +16,6 @@ const userSchema = new mongoose.Schema({
   password: String,
 });
 
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"],
-});
 const User = mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
@@ -37,7 +33,7 @@ app.get("/register", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password,
+    password: md5(req.body.password),
   });
 
   newUser.save().then(() => {
@@ -47,7 +43,7 @@ app.post("/register", (req, res) => {
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({ email: username })
     .then((data) => {
