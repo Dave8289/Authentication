905cc1e8fc268976c2aa542d4c3a547696b5dacf
Davinder Kumar <davindertwal@gmail.com>
2023-06-08 20:28:46 +0530

    level 4 bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 mongoose.connect("mongodb://localhost:27017/userDB");
 
 const app = express();
@@ -31,27 +32,31 @@ app.get("/register", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password),
-  });
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash,
+    });
 
-  newUser.save().then(() => {
-    res.render("secrets");
+    newUser.save().then(() => {
+      res.render("secrets");
+    });
   });
 });
 
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({ email: username })
     .then((data) => {
-      if (data.password === password) {
-        res.render("secrets");
-      } else {
-        res.send("Wrong password");
-      }
+      bcrypt.compare(password, data.password).then((resp) => {
+        if (resp) {
+          res.render("secrets");
+        } else {
+          res.send("Wrong password");
+        }
+      });
     })
     .catch((err) => {
       res.send(err);
